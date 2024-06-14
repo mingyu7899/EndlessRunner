b81bb2d80db33ee8ccfba0bbcc5c1caea2c788b2
leedongkyu <96910855+leedonggyu0526@users.noreply.github.com>
2024-06-14 18:32:06 +0900

    first
    first

---
+++ EndlessRunner/Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour {
+
+    [SerializeField] GameObject[] groundTile;
+    Vector3 nextSpawnPoint;
+
+    public void SpawnTile (bool spawnItems)
+    {
+        int randomIndex = Random.Range(0, groundTile.Length);
+        GameObject temp = Instantiate(groundTile[randomIndex], nextSpawnPoint, Quaternion.identity);
+        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+
+        if (spawnItems) {
+            temp.GetComponent<GroundTile>().SpawnObstacle();
+            temp.GetComponent<GroundTile>().SpawnCoins();
+        }
+    }
+
+    private void Start () {
+        for (int i = 0; i < 3; i++) {
+            SpawnTile(true);
+        }
+    }
+}
